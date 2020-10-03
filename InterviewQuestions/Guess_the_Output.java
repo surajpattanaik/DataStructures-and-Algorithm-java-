be8e616f179bea56435dd634ce4d31bf329b7a30
Suraj Kumar pattanaik <44063901+surajpattanaik@users.noreply.github.com>
2020-10-03 23:48:23 +0530

    Add files via upload

---
+++ InterviewQuestions/Guess_the_Output.java
@@ -0,0 +1,14 @@
+class Guess_the_Output
+{
+	public static void main(String args[]){
+		int i = 1; 
+		int j = 1;
+		while(++i<=5){
+			while(++j<=5){
+				System.out.println(j);
+			}
+			System.out.println(j);
+
+		}
+	}
+}
