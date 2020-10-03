5e2b87e715a298c713a9bba90914fb22cae8997d
Suraj Kumar pattanaik <44063901+surajpattanaik@users.noreply.github.com>
2020-10-03 23:46:08 +0530

    check whether two strings are Anagrams

---
+++ InterviewQuestions/Anagram.java
@@ -0,0 +1,34 @@
+class Anagram
+{
+	public static void main(String args[]){
+		String a = "aaabbbc";
+		String b = "bbaacab";
+		int al[] = new int[256];
+		int bl[] = new int[256];
+		boolean isAnagram = true;
+		for (char c:a.toCharArray() )
+		{
+			int index =(int)c;
+			al[index]++;
+		}
+        for (char c:b.toCharArray() )
+		{
+			int index =(int)c;
+			bl[index]++;
+		}
+		for (int i=0; i<256 ;i++ )
+		{
+			if (al[i] != bl[i])
+			{
+				isAnagram = false;
+				break;
+			}
+		}
+		if(isAnagram) {
+		System.out.println("Anagram");
+		}
+		else
+        System.out.println("not an Anagram");
+
+	}
+}
