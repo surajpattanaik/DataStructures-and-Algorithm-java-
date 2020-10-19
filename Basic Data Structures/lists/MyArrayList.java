d2f4f382587ed588f13762ad37cecaf78efe87fe
Suraj Kumar pattanaik <44063901+surajpattanaik@users.noreply.github.com>
2020-10-19 22:52:09 +0530

    Add files via upload

--- Basic Data Structures/lists/MyArrayList.java
+++ Basic Data Structures/lists/MyArrayList.java
@@ -1,26 +1,63 @@
 package lists;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
-public class MyArrayList {
+import java.util.ArrayList;
+
+public class MyArrayList extends ArrayList {
+
+	static ArrayList<Object> fruits = new MyArrayList();
 
 	public static void main(String[] args) {
 
-		List<String> fruits = new LinkedList();
+		// method to add the element in the list
+		System.out.println(fruits.add("S1"));
+		System.out.println(fruits.add("S2"));
+		System.out.println(fruits.add("S3"));
+
+		System.out.println(fruits.add("S4"));
+		System.out.println(fruits.add("sss"));
+		System.out.println(fruits.add("zzz"));
+		System.out.println(fruits.add("lll"));
+		System.out.println(fruits.add("sss"));
+		System.out.println(fruits.add(5));
+		System.out.println(fruits.add(6));
+		System.out.println(fruits.add(5));
+		System.out.println(fruits.add("s4"));
 		
-		//method to add the element in the list
-		fruits.add("Apple");
-		fruits.add("Orange");
-		fruits.add("Hi");
 
-		String temp[] = new String[fruits.size()];
+		System.out.println();
+		for (Object o : fruits) {
+			System.out.print(" " + o);
+		}
 
-		fruits.toArray(temp);
+		((MyArrayList) fruits).deleteDuplicates();
 
-		for (String e : temp) {
-			System.out.println(e);
+		System.out.println();
+		for (Object o : fruits) {
+			System.out.print(" " + o);
 		}
 	}
+
+	// method to delete all duplicate values from ArrayList
+	public void deleteDuplicates() {
+		for (int i = 0; i < fruits.size(); i++) {
+			for (int j = i + 1; j < fruits.size(); j++) {
+
+				if (fruits.get(i).equals(fruits.get(j))) {
+					fruits.remove(j);
+
+				}
+			}
+		}
+	}
+	/*
+	 * //method to stop adding duplicate values
+	 * 
+	 * @Override 
+	 * public boolean add(Object e) {
+	 * if (!fruits.contains(e)) { return super.add(e); } else { return false; }
+	 * 
+	 * 
+	 * }
+	 */
 
 }
